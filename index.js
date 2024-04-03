9e0b08beb175480047228b196c471182db842409
Yin <doyin@deloitte.com.cn>
2024-04-03 14:15:19 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberArrayToString = require('./numberArrayToString')
+const isEven = require('./isEven')
+const isPalindrome = require('./isPalindrome')
+
+module.exports = (num) => {
+    return num % 2 !== 0;
+  }
